c50bef11efa92ea14ae2c59b47072f3e8adb81f0
ckfu37186 <ckfu37186@gmail.com>
2024-12-19 21:27:01 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY EVIL4REAL
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7442188342:AAGdEnwU7j7SWHe2qmnxUREwGfzbdcCeo-g")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@xdtop0")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5993004087")
+
+
+
+
+
+
+
+  
